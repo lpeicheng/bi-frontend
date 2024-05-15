80a95469dc9c0dee05059645374e305f19591060
itcast <hello@itcast.cn>
2024-05-16 01:22:12 +0800

    增加dockerfile

---
+++ config/myaxios.ts
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+const isDev = process.env.NODE_ENV === 'development';
+
+const myAxios = axios.create({
+  baseURL: isDev ? 'http://localhost:8101/api' : 'http://106.52.221.21/api',
+})
+
+myAxios.defaults.withCredentials = true; //设置为true
+
+// Add a request interceptor
+myAxios.interceptors.request.use(function (config) {
+  console.log('我要发请求啦')
+  return config;
+}, function (error) {
+  return Promise.reject(error);
+});
+
+myAxios.interceptors.response.use(function (response) {
+  console.log('我收到你的响应啦')
+  // 未登录则跳转登录页
+  if (response?.data?.code === 40100) {
+    const redirectUrl = window.location.href;
+    window.location.href = `/user/login?=redirect=${redirectUrl}`;
+  }
+  return response.data;
+}, function (error) {
+  // Do something with response error
+  return Promise.reject(error);
+});
+
+export default myAxios;
